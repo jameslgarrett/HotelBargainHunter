6db3213d351b75db4a1f53dc13bf412b2a09e7eb
James Garrett <jgarrett@comply365.com>
2015-05-19 19:13:36 -0500

    Initial upload, almost zero testing has been completed.

---
+++ MarriottScraper/Hotel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBargainHunter
+{
+    class Hotel
+    {
+        public String name;
+        public String address;
+        public String distance;
+        public String price;
+        public String hotelsComPrice;
+        public String streetAddress;
+        public String city;
+        public String zipCode;
+        public bool marriottMatch = false;
+
+        public Hotel()
+        {
+
+        }
+
+        public Hotel(String name, String address, String distance, String price)
+        {
+            this.name = name;
+            this.address = address;
+            this.distance = distance;
+            this.price = price;
+        }
+    }
+}
